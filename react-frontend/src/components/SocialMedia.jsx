9ee00782add63da382be79e3bf16c7739ddb4375
Shubham Kamble <shubhamkamble8983@gmail.com>
2025-11-16 21:09:32 +0530

    moved portfolio to seperate repo from projects repo

---
+++ react-frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      <a
+        href="https://www.linkedin.com/in/shubhamkamble89/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <FaLinkedinIn />
+        </div>
+      </a>
+      <a
+        href="https://twitter.com/shubhamkk89"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <BsTwitter />
+        </div>
+      </a>
+      <a
+        href="https://www.instagram.com/mr.cool_shubz/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>
+          <BsInstagram />
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default SocialMedia;
